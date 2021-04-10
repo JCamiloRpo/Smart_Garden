21fba7379903d31a74a7e77685b5d7235cbb3e14
wilder3756 <60258334+wilder3756@users.noreply.github.com>
2021-04-10 18:37:15 -0500

    Prueba Sensor intensidad de luz (luxometro)
    Se utiliza el siguiente tutorial https://www.youtube.com/watch?v=AoFsWAJ-pE0

---
+++ Infraestructura/Arduino/EjemplosIndividuales/Luxometro_BH1750/Luxometro_BH1750.ino
@@ -0,0 +1,23 @@
+//Tutorial: https://www.youtube.com/watch?v=AoFsWAJ-pE0
+
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 Luxometro;
+
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("Inicializando sensor...");
+  Luxometro.begin(); //inicializamos el sensor
+}
+
+
+void loop() {
+  uint16_t lux = Luxometro.readLightLevel();//Realizamos una lectura del sensor
+  Serial.print("Luz(iluminancia):  ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+}
