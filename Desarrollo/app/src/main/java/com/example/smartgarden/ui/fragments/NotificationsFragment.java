50909d858b4ab80f9aa12f7e54e856f6e72293e2
J Camilo Rpo V <60360279+CarolinaMons@users.noreply.github.com>
2021-05-02 06:18:09 -0500

    Ajustes

--- Desarrollo/app/src/main/java/com/example/smartgarden/ui/fragments/NotificationsFragment.java
+++ Desarrollo/app/src/main/java/com/example/smartgarden/ui/fragments/NotificationsFragment.java
@@ -16,9 +16,7 @@ import com.example.smartgarden.R;
 
 public class NotificationsFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         textView.setText("Ventana de notificaciones");
---
+++ Desarrollo/app/src/main/java/com/example/smartgarden/ui/fragments/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.example.smartgarden.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.smartgarden.R;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
